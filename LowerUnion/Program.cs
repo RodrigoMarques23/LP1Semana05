c630ec21fea71ca109ce2e89b0af0f82ae5f2a9b
Butchsss <rodrigomarques23work@gmail.com>
2023-03-31 13:55:53 +0100

    XML add - solved

--- LowerUnion/Program.cs
+++ LowerUnion/Program.cs
@@ -6,17 +6,25 @@ namespace LowerUnion
     {
         static void Main(string[] args)
         {
-            if (args == null)
+
+            if (args.Length == 0)//verify array length
             {
-                Console.WriteLine("sem qualquer argumento!");
+                Console.WriteLine("sem qualquer argumento!");//print info
             }
-            else
+            else//if array>0 
             {
+                //convert all strings to lowercase
                 for (int i = 0; i < args.Length; i++)
                 {
                     args[i] = args[i].ToLower();
                 }
-                Array.Sort(args);
+                Array.Sort(args);//sort alphabetic
+                //print all args with a underscore after each string
+                for (int x = 0; x < args.Length; x++)
+                {
+                    Console.Write(args[x]);//print string
+                    Console.Write("_");//print underscore after string
+                }
             }
         }
     }
