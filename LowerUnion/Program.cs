77cae4d8743b39c8094b3fee6c5c6eeb9f115aaa
Butchsss <rodrigomarques23work@gmail.com>
2023-03-31 13:47:23 +0100

    Condition and args to lower add

---
+++ LowerUnion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LowerUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                Console.WriteLine("sem qualquer argumento!");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    args[i] = args[i].ToLower();
+                }
+            }
+        }
+    }
+}
